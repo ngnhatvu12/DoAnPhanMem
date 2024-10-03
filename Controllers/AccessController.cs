1442a6c1a3728e8b5f42b337d4692089472bec22
ngocquyen <quyen31415926@gmail.com>
2024-10-03 13:30:48 +0700

    Xu ly Dang Nhap

---
+++ Controllers/AccessController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DoAnPhanMem.Models;
+namespace DoAnPhanMem.Controllers
+{
+    public class AccessController : Controller
+    {
+        private readonly WebTheThaoDbContext _db;
+
+        public AccessController(WebTheThaoDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]        
+        public IActionResult Login()
+        {
+                return View();
+        }
+        [HttpPost]
+        public IActionResult Login(TaiKhoan taiKhoan)
+        {
+            if (HttpContext.Session.GetString("TenDangNhap") == null)
+            {
+                var u = _db.TaiKhoan.Where(x => x.TenDangNhap.Equals(taiKhoan.TenDangNhap) &&
+                x.MatKhau.Equals(taiKhoan.MatKhau)).FirstOrDefault();
+                if (u != null)
+                {
+                    HttpContext.Session.SetString("TenDangNhap", u.TenDangNhap.ToString());
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+    }
+}
